163b67180feeed9ab1ee6967ddfb1decd448a7d2
kilango <mankilla12345@gmail.com>
2021-05-17 17:37:57 +0300

    first added changes

---
+++ src/components/header/header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Heade = () => {  
+    return (
+      <div> textInComponent </div>
+    ) 
+}
+
+export default Header;
---
+++ src/hoc/layout/layout.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './layout.css';
+
+export default class Layout extends Component {
+  render() {
+    return (
+    
+      <div> 
+        Header
+        {this.props.children}
+        Footer 
+      </div>
+
+    );
+  }
+}
+ 
